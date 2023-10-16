56d694ef24ac9965b7be54b4552e1baa005deb02
PetrPalych <a.shamilbek.inai@gmail.com>
2023-10-16 11:21:48 +0600

    Add lab2

--- book/urls.py
+++ book/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('book/', view_book),
+    path('', view_book),
+    path('<int:id>', detail_view)
 ]
--- book/views.py
+++ book/views.py
@@ -8,3 +8,8 @@ from .models import *
 def view_book(request):
     queryset = Book.objects.all()
     return render(request, 'book_view.html', {'books': queryset})
+
+
+def detail_view(request, id):
+    queryset = Book.objects.get(id=id)
+    return render(request, 'book_detail.html', {"book": queryset})
