f6b30db1c5c47a4e6fd451ac1afd6aa51277d6af
PetrPalych <a.shamilbek.inai@gmail.com>
2023-11-04 08:52:48 +0600

    crud

--- book/urls.py
+++ book/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_book),
-    path('book_detail/<int:id>/', views.detail_view),
-    path('add-comment/', views.create_review_for_book),
-    path('create_book/', views.create_view_book_post),
-    path('book_list/<int:id>/delete/', views.view_book_drop),
-    path('book_list/', views.view_book_delete),
+    path('', views.BookListView.as_view()),
+    path('book_list/', views.BookDeleteListView.as_view()),
+    path('book_detail/<int:pk>/', views.BookDetailView.as_view()),
+    path('add-comment/', views.ReviewCreateView.as_view()),
+    path('create_book/', views.BookCreateView.as_view()),
+    path('book_list/<int:pk>/delete/', views.BookDeleteView.as_view()),
+    path('book_list/<int:pk>/update/', views.BookUpdateView.as_view()),
 ]
--- book/views.py
+++ book/views.py
@@ -1,6 +1,7 @@
 from django.forms import forms
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView, CreateView, DeleteView
 
 from . import models, forms
 
@@ -8,49 +9,48 @@ from . import models, forms
 # Create your views here.
 
 
-def view_book(request):
-    book_value = models.Book.objects.all()
-    return render(request, "book_view.html", {"book_key": book_value})
+class BookListView(ListView):
+    model = models.Book
+    queryset = models.Book.objects.all()
+    context_object_name = "books"
+    template_name = "book_view.html"
 
 
-def detail_view(request, id):
-    book_id = get_object_or_404(models.Book, id=id)
-    return render(request, 'book_detail.html', {'book_key': book_id})
+class BookDeleteListView(ListView):
+    model = models.Book
+    queryset = models.Book.objects.all()
+    context_object_name = "book_key"
+    template_name = "book_list.html"
 
 
-def create_review_for_book(request):
-    method = request.method
-    if method == 'POST':
-        form = forms.ReviewForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-
-            return HttpResponse("<a href='/'>Назад</a>")
-    else:
-        form = forms.ReviewForm()
-
-    return render(request, 'create_review_for_book.html', {'form': form})
+class BookDetailView(DetailView):
+    model = models.Book
+    queryset = models.Book.objects.all()
+    context_object_name = "book"
+    template_name = "book_detail.html"
 
 
-def create_view_book_post(request):
-    method = request.method
-    if method == "POST":
-        form = forms.BookForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponse('Добавлен успешно!')
-    else:
-        form = forms.BookForm()
-    return render(request, 'create_book.html', {'form': form})
+class BookCreateView(CreateView):
+    model = models.Book
+    form_class = forms.BookForm
+    success_url = "/"
+    template_name = "create_book.html"
 
 
-def view_book_drop(request, id):
-    lang_id = get_object_or_404(models.Book, id=id)
-    lang_id.delete()
-    return HttpResponse('Удален успешно!')
+class ReviewCreateView(CreateView):
+    model = models.ReviewBook
+    form_class = forms.ReviewForm
+    success_url = "/"
+    template_name = "create_review_for_book.html"
 
 
-def view_book_delete(request):
-    lang_value = models.Book.objects.all()
-    return render(request, 'book_list.html', {'book_key': lang_value})
+class BookDeleteView(DeleteView):
+    model = models.Book
+    success_url = "/"
 
+
+class BookUpdateView(CreateView):
+    model = models.Book
+    form_class = forms.BookForm
+    success_url = "/"
+    template_name = "book_update.html"
